0d9d04123cb59cf1dc1f28aef5fcb23e42d82f9a
Feynman Liang <feynman.liang@gmail.com>
2016-02-28 20:06:58 +0000

    Updates writeup

--- make-tar.zsh
+++ make-tar.zsh
@@ -1,2 +1,2 @@
 ./squash --genCodebooks < filep.01.10000NR >/dev/null
-tar czf Liang_Feynman-MLSALT9-Coursework-2.tar.gz HuffmanEncoder.py squash unsquash geometric_codebook.pickle
+tar czf Liang_Feynman-MLSALT9-Coursework-2.tar.gz HuffmanEncoder.py squash unsquash geometric_codebook.pickle ./writeup/notes.pdf
